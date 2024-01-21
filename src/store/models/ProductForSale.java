b8b7954757db51df3745054c182b725d13464126
didaryalcin <ddrylcn@gmail.com>
2024-01-21 03:59:42 +0300

    s14d4

---
+++ src/store/models/ProductForSale.java
@@ -0,0 +1,26 @@
+package store.models;
+
+public class ProductForSale {
+    private String type;
+    private double price;
+    private String description;
+
+
+    public ProductForSale(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
